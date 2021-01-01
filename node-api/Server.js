c073f152762dd5a01bbfe67f43b991c493883ce2
Edgar Amado <edgjesus@gmail.com>
2021-01-01 16:20:15 +0200

    Update Server.js Jan 1st

--- node-api/Server.js
+++ node-api/Server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
 //Inicializando o App
 const app = express();
 
 //Inicializando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    { useNewUrlParser:true, useUnifiedTopology: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model("Product");
 
 //Criando rotas
 app.get('/', (req, res) => {
-    res.send('Hello nodemon');
+    Product.create({
+        title: "ReactJs",
+        description: "Build powerful sites with React",
+        url: "http://github.com/facebook/react-native"
+    });
+    return res.send('Hello nodemon');
 });
 
 app.listen(3333);
