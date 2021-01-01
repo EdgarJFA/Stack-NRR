cc6e817359913d5031e19263196c101527ef6bfa
unknown <edgjesus@gmail.com>
2021-01-01 12:09:14 +0200

    Add Mongoose DB Jan 1st

--- node-api/Server.js
+++ node-api/Server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Inicializando o App
 const app = express();
 
+//Inicializando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser:true, useUnifiedTopology: true });
+
+//Criando rotas
 app.get('/', (req, res) => {
     res.send('Hello nodemon');
 });
