9b1b095b095e493007de6b8f481827c47c61ba7e
EdgarJFA <62776977+EdgarJFA@users.noreply.github.com>
2021-01-01 10:14:47 +0200

    Update Server Jan 1st

--- node-api/Server.js
+++ node-api/Server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat');
+    res.send('Hello nodemon');
 });
 
 app.listen(3333);
