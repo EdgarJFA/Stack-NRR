6c3a742be50a87fd6d740a67dbf20155088c2bf4
Edgar Amado <edgjesus@gmail.com>
2021-01-02 19:52:45 +0200

    Add CRUD Jan 1

--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -4,5 +4,9 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 //Criando rotas
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
