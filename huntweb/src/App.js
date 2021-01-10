abba5d4cd06541ff3a25c4117b378474f932f499
Edgar Amado <edgjesus@gmail.com>
2021-01-10 16:43:32 +0200

    Updated App.Js Jan 10th

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,13 +1,13 @@
 
 import Header from './components/Header';
-import Main from './pages/main';
+import Routes from './routes';
 import './global.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Routes/>
     </div>
   );
 }
